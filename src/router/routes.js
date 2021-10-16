aa0c464d49c443a262512610281e6ee7ec560995
maxim shparik <maksim.s@devstark.com>
2021-10-16 17:55:24 +0200

    try fix routing in prod

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,49 +3,49 @@ const metaPublic = { layout: 'LayoutDefault' }
 // const metaAccount = { layout: 'LayoutDefault', auth: true }
 export default [
   {
-    path: '/',
+    path: process.env.NODE_ENV === 'production' ? '/rb_shop/' : '/',
     name: 'page-main',
     component: () => import('@/pages/PageMain'),
     meta: metaPublic
   },
   {
-    path: '/all',
+    path: process.env.NODE_ENV === 'production' ? '/rb_shop/' : '/all',
     name: 'page-goods',
     component: () => import('@/pages/PageGoods'),
     meta: metaPublic
   },
   {
-    path: '/news',
+    path: process.env.NODE_ENV === 'production' ? '/rb_shop/' : '/news',
     name: 'page-news',
     component: () => import('@/pages/PageNews'),
     meta: metaPublic
   },
   {
-    path: '/wishlist',
+    path: process.env.NODE_ENV === 'production' ? '/rb_shop/' : '/wishlist',
     name: 'page-wishlist',
     component: () => import('@/pages/PageWishlist'),
     meta: metaPublic
   },
   {
-    path: '/checkout',
+    path: process.env.NODE_ENV === 'production' ? '/rb_shop/' : '/checkout',
     name: 'page-checkout',
     component: () => import('@/pages/PageCheckout'),
     meta: metaPublic
   },
   {
-    path: '/item/:id',
+    path: process.env.NODE_ENV === 'production' ? '/rb_shop/' : '/item/:id',
     name: 'page-item',
     component: () => import('@/pages/PageItem'),
     meta: metaPublic
   },
   {
-    path: '/profile',
+    path: process.env.NODE_ENV === 'production' ? '/rb_shop/' : '/profile',
     name: 'page-profile',
     component: () => import('@/pages/PageProfile'),
     meta: metaPublic
   },
   {
-    path: '/*',
+    path: process.env.NODE_ENV === 'production' ? '/rb_shop/' : '/*',
     name: 'page-404',
     component: () => import('@/pages/404'),
     meta: metaPublic
